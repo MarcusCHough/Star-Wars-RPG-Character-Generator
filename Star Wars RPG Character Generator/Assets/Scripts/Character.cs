b84625ae90ed8e3b5d10a9cf06c520f1b06f295a
ConqueringBoardom <37284640+ConqueringBoardom@users.noreply.github.com>
2020-01-18 19:26:02 -0600

    Initial Commit
    Exluded Paid Asset Packages.
    
    Required Packages:
    Sci-Fi UI Components Pack
    SnapToGrid

---
+++ Star Wars RPG Character Generator/Assets/Scripts/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets
+{
+    class Character
+    {
+        Dictionary<string, int> characteristics;
+        Dictionary<Enum, int> skills;
+        Dictionary<int, Item> equipment;
+        Dictionary<int, Weapon> weapons;
+        Armor armor;
+    }
+}
